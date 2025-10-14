935e22b2c6e1e0589749f001895b34635cd2955b
HoChan8253 <nahcohochan@gmail.com>
2025-10-14 18:55:20 +0900

    [Add]

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float moveSpeed = 5f;
+   
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(h, v, 0);
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
     }
 }
